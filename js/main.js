0d0d02c153983dfe70d2af5946801c5dd3a2d5a8
wp <weipan789@gmail.com>
2017-11-24 13:58:45 +0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,44 @@
+$(function () {
+    var $sidebarToggle=$('<button id="sidebar-toggle" class="sidebar-toggle"><div class="sidebar-toggle-button"><span></span><span></span><span></span></div></button>');
+    $('body').append($sidebarToggle);
+    $sidebarToggleButton = $sidebarToggle.find('.sidebar-toggle-button');
+    var $tableOfContents=$('#table-of-contents');
+    $tableOfContents.addClass('sidebar').prependTo($('body'));
+    var $sidebarNav = $('#text-table-of-contents');
+    $sidebarNav.addClass('sidebar-nav');
+
+    $sidebarNav.find('li a').addClass('section-link');
+    $sidebarNav.find('li ul').addClass('children');
+
+    $('body').append('<div id="main-content"></div>');
+    var $mainContent = $('#main-content');
+    var outestIds = ['table-of-contents','main-content','sidebar-toggle'];
+    $('body').children().each(function () {
+        var id = $(this).attr('id');
+        if(outestIds.indexOf(id)==-1) {
+            $(this).appendTo($mainContent);
+        }
+    });
+
+    //显示与隐藏
+    $sidebarToggleButton.bind('click',function () {
+        if($tableOfContents.is(":hidden")) {
+            $tableOfContents.show();
+            $mainContent.css('left','300px');
+            $('body').removeClass('close');
+        }else {
+            $tableOfContents.hide();
+            $mainContent.css('left','0px');
+            $('body').addClass('close');
+        }
+    });
+
+    //选中
+    $sidebarNav.find('li').bind('click',function () {
+        $sidebarNav.find('li').removeClass('active');
+        $(this).addClass('active');
+    });
+
+
+    // the end
+})
